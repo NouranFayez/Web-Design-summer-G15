f19c1f9dbf5a5d22687cd477b2e1169aa2cbd12c
NouranFayez <nouran_ahmed269@hotmail.com>
2026-08-27 13:46:05 +0300

    session9

---
+++ session9 - part 2/js/script.js
@@ -0,0 +1,105 @@
+console.log("javascript")
+
+// var let const
+
+// var variableName = value
+
+var userName = "Zein"
+var lastName = "Tamer"
+// concatenate
+var fullName = "user name = "+ userName + " " + lastName
+var userAge = 20
+
+// template string
+var fullName = `user name  = ${userName} ${lastName}
+user age = ${userAge}`
+
+
+
+// console.log(fullName)
+
+// console.log(userName)
+// console.log(userAge)
+
+var isUserGraduated = false
+
+
+
+var age 
+
+// console.log(age)
+
+// console.log(isUserGraduated)
+
+var h = null
+
+
+// var p = prompt("enter your name")
+// name => string
+//  ok => empty string 
+// cancel ==> null
+
+// console.log(p)
+
+
+// Mathematical Operators ==> + - / * ** % ++ --
+
+var num1 = 20
+var num2 = 30
+var num3 = "50"
+var num4 = "sdfghj"
+var sum = num1 + num2 + num3
+var sum = num3 + num2 + num1
+// console.log(sum)
+
+// console.log(num2 - num1)
+// console.log(num1 / num3)
+// console.log(num1 / num4) // NaN ==> Not a Number
+
+/// ++ increment 
+var x = 20
+// console.log(x++) // 20
+// console.log(x) // 21
+// console.log(++x) // 22
+
+// console.log("-------------------")
+
+/// -- decrement 
+// console.log(x--) // 22
+// console.log(x) // 21
+// console.log(--x) // 20
+
+
+
+var h = 30
+h = h + 5 // 35
+h+=10 // 45
+// console.log(h)
+
+
+// console.log(typeof(h))
+// console.log(typeof num4)
+
+// comparison operator ==>  > >= < <= == === != !==
+
+
+var k = 20
+var t = 20
+var s = 30
+var m = "30"
+
+console.log(s > t)
+console.log(k >= t)
+
+console.log(s == m) // value ==> true
+console.log(s === m) // value - data type ==> false
+
+console.log(typeof m == "string")
+console.log(typeof m == "String")
+
+
+console.log(s != m) // value ==> false
+console.log(s !== m) // value - data type ==>  true
+
+
+
