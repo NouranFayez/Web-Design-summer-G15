c3e19857896d0baa10ce6fe673dbf3d9758668de
NouranFayez <nouran_ahmed269@hotmail.com>
2026-08-30 13:51:10 +0300

    session10

--- session10/js/script.js
+++ session10/js/script.js
@@ -185,18 +185,18 @@ var test; // undefined - false
 // sum()
 
 
-function sum(num1  , num2 , num3 = 200){
-    var n = num1
-    var m = num2
-    var result = n + m 
+// function sum(num1  , num2 , num3 = 200){
+//     var n = num1
+//     var m = num2
+//     var result = n + m 
 
-    // console.log(num3)
-    // return result
-    // return "js"
-    return m+n + num3
-//    return n > m
+//     // console.log(num3)
+//     // return result
+//     // return "js"
+//     return m+n + num3
+// //    return n > m
    
-}
+// }
 // sum()
 
 // var res = sum(100 , 50 , 70 )
@@ -207,11 +207,122 @@ function sum(num1  , num2 , num3 = 200){
 
 
 
-function test(msg){
-    var p = prompt(msg);
-    return p
-}
+// function testPrompt(msg){
+//     var p = prompt(msg);
+    
+// }
 
-var userName = test("enter your name")
-var userAge = test('enter your age')
-var userAddress = test('enter your address')
+// var userName = testPrompt('enter your name')
+// var userAge = testPrompt('enter your age')
+// var userAddress = testPrompt('enter your address')
+
+
+// function looping ==> startNum , endNum , breakNum , contNum
+
+// looping(1)
+// looping(1 , 10)
+// looping(1 , 10 , 3)
+// looping(1 , 10 , 8 , 3)
+
+
+
+
+////////// function statment 
+function funcName(){
+    return "Javascript"
+}
+var t = funcName()
+// console.log(t)
+
+///////// function expression
+var info = function(){
+    return "JS"
+}
+// console.log(info())
+
+///////// Arrow Function
+// var output = (num)=>{
+//     return num > 20
+// }
+var output = (num)=> num > 20
+
+
+
+// console.log(output(30))
+
+
+
+///////// Array
+
+// var userName = "Zein"
+// var userAge = 20
+// var userAddress = "Maadi"
+// var isUserGraduated = false
+// var userNationality = "Egyptian"
+
+var userInfo = ["Zein" , 20 , "Maadi" , false , "Egyptian"]
+
+console.log(userInfo[2]) // get item
+
+userInfo[5] = "student"
+console.log(userInfo.length)
+
+userInfo[userInfo.length] = "fghjk"
+
+
+userInfo[1] = 21
+
+////////////////// Methods of array
+
+userInfo.push("Nasr city" , "010000000" )
+userInfo.unshift("web design")
+userInfo.shift()
+userInfo.pop()
+
+// splice(index , deletedCount , item/s) ==> add / remove elements
+
+userInfo.splice(6 , 1)
+userInfo.splice(3 , 0 , "october" , true)
+userInfo.splice(7 , 1 , 'Developer')
+
+console.log(userInfo)
+console.log(userInfo.includes("Maadi"))
+console.log(userInfo.includes("new cairo"))
+
+console.log(userInfo.indexOf("Egyptian"))
+console.log(userInfo.toString())
+
+
+// console.log(userInfo[0])
+// console.log(userInfo[1])
+// console.log(userInfo[2])
+// console.log(userInfo[3])
+// console.log(userInfo[4])
+// console.log(userInfo[5])
+// console.log(userInfo[6])
+// console.log(userInfo[7])
+// console.log(userInfo[8])
+// index ==> 0: 8
+// length ==> 9
+console.log("---------------------------")
+
+for(var i = 0 ; i < userInfo.length ; i++ ){
+    console.log(userInfo[i])
+}
+console.log("---------------------------")
+
+userInfo.forEach((element , index , arr)=>{
+    console.log(`index = ${index} , element = ${element}`)
+    console.log(arr)
+    // console.log('js')
+})
+
+// ()=>{
+
+// }
+
+
+var  courses = ["html" , "css" , "js"]
+
+
+
