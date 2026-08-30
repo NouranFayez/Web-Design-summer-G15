dacfd4c2f7d5eacf9892978a08a7531a0d9e8d97
NouranFayez <nouran_ahmed269@hotmail.com>
2026-08-30 11:32:32 +0300

    session10

--- session10/js/script.js
+++ session10/js/script.js
@@ -49,7 +49,7 @@ var test; // undefined - false
 // }
 
 
-var p = prompt('enter your job')
+// var p = prompt('enter your job')
 // if(p == "dev"){
 //     var result = "developer" 
 //     console.log(result)
@@ -65,41 +65,41 @@ var p = prompt('enter your job')
 // doc  ==> doctor   
 
 
-if (p == null || p == "") {
-    alert('please enter your job')
-}
-else if (p == "dev" || p == "DEV") {
-    var result = "developer"
-    console.log(result)
+// if (p == null || p == "") {
+//     alert('please enter your job')
+// }
+// else if (p == "dev" || p == "DEV") {
+//     var result = "developer"
+//     console.log(result)
 
-}
-else if (p == "doc" || p == "dr") console.log('doctor')
-else if (p == "eng") console.log('engineer')
-else {
-    console.log('invalid')
+// }
+// else if (p == "doc" || p == "dr") console.log('doctor')
+// else if (p == "eng") console.log('engineer')
+// else {
+//     console.log('invalid')
 
-}
+// }
 
 // ok ==> ""
 
-if (p == null || p == "") {
-    alert("please enter your job")
-}
-else {
-    if (p == "dev" || p == "DEV") {
-        var result = "developer"
-        console.log(result)
+// if (p == null || p == "") {
+//     alert("please enter your job")
+// }
+// else {
+//     if (p == "dev" || p == "DEV") {
+//         var result = "developer"
+//         console.log(result)
 
-    }
-    else if (p == "doc" || p == "dr") console.log('doctor')
-    else if (p == "eng") console.log('engineer')
-    else {
-        console.log('invalid')
+//     }
+//     else if (p == "doc" || p == "dr") console.log('doctor')
+//     else if (p == "eng") console.log('engineer')
+//     else {
+//         console.log('invalid')
 
-    }
-}
+//     }
+// }
 
-var degree = prompt("enter your degree")
+// var degree = prompt("enter your degree")
 // cancel , ok (empty string) ==> alert 
 // > 100 ==> invalid degree
 // > 90  ==> Excellent
@@ -109,4 +109,109 @@ var degree = prompt("enter your degree")
 
 
 
+// switch case  ==> === value , data type
 
+// var job = 20
+// switch (job) {
+//     case "Developer":
+//     case "developer"  :   
+//         console.log('Developer')
+//         break;
+//     case "engineer":
+//         console.log('engineer')
+//         break;
+//     case "Accountant":
+//         console.log('Accountant')
+//         break;
+//     case "20" :
+//         console.log('test case')   
+//        break;  
+//     default : console.log('invlid')    
+
+// }
+
+
+// console.log(1)
+// console.log(2)
+// console.log(3)
+// console.log(4)
+// console.log(5)
+// console.log(6)
+// console.log(7)
+// console.log(8)
+
+
+// for(initialization ; condition ; increment/decrement){
+//     code
+// }
+
+// for(var i = 1 ; i <= 10 ; i++){
+ 
+//     // if(i == 3) console.log('number = 3')
+//     if(i == 3) {
+//         continue
+//     }
+//     else if(i == 9) break
+//        console.log(i)
+// }
+
+// for(var i = 10 ; i >=  0 ; i-=2){
+//     console.log(i)
+// }
+
+
+
+// for(var i = 1 ; i<= 5 ; i++){
+//     alert(`number = ${i}`)
+// }
+
+
+
+// prompt()
+
+
+// function ==> name 
+//              body
+//              call
+//              parameters
+//              return
+
+
+// function sum(){
+//     var n = 20
+//     var m = 30
+//     console.log(n + m)
+// }
+// sum()
+
+
+function sum(num1  , num2 , num3 = 200){
+    var n = num1
+    var m = num2
+    var result = n + m 
+
+    // console.log(num3)
+    // return result
+    // return "js"
+    return m+n + num3
+//    return n > m
+   
+}
+// sum()
+
+// var res = sum(100 , 50 , 70 )
+
+// console.log(res)
+
+// sum()
+
+
+
+function test(msg){
+    var p = prompt(msg);
+    return p
+}
+
+var userName = test("enter your name")
+var userAge = test('enter your age')
+var userAddress = test('enter your address')
