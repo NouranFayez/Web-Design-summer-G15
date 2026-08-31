48a96ebd1ddb0eb3ee286be685e7e25ed8925be0
NouranFayez <nouran_ahmed269@hotmail.com>
2026-08-31 10:35:53 +0300

    session11

--- session11/js/script.js
+++ session11/js/script.js
@@ -1,10 +1,10 @@
-var nums = [2 , 10 , 50 , 5 , 20]
+var nums = [2, 10, 50, 5, 20]
 
-nums.forEach((item , i , arr)=>{
-    if(item > 10){
+nums.forEach((item, i, arr) => {
+    if (item > 10) {
         // console.log(item)
         // console.log(i)
-    } 
+    }
 })
 
 // nums.find((item)=>{
@@ -12,15 +12,15 @@ nums.forEach((item , i , arr)=>{
 // })
 // console.log(nums.find((item)=> item > 10))
 // find , findIndex , filter ==> (> >= < <= == === != !==)
-var num = nums.find((item)=> item > 100) // undefined
-var num = nums.find((item)=> item > 10)
-var index = nums.findIndex((item)=> item > 100) // -1
-var index = nums.findIndex((item)=> item > 10)
+var num = nums.find((item) => item > 100) // undefined
+var num = nums.find((item) => item > 10)
+var index = nums.findIndex((item) => item > 100) // -1
+var index = nums.findIndex((item) => item > 10)
 
 var newArray = nums.filter(num => num > 5)
 var newArray = nums.filter(num => num > 100)
 
-var mapArray = nums.map(num => num*2)
+var mapArray = nums.map(num => num * 2)
 // console.log(nums)
 // console.log(mapArray)
 
@@ -35,40 +35,98 @@ var mapArray = nums.map(num => num*2)
 
 var lastName = "Mazen"
 var user = {
-    firstName : "zein" ,
-    lastName : "tamer" ,
-    age : 30 ,
-    isGraduated : true,
-    fullName : function(){
+    firstName: "zein",
+    lastName: "tamer",
+    age: 30,
+    isGraduated: true,
+    fullName: function () {
         // this ==> object (user)
         return `${this.firstName} ${this.lastName}`
     },
-    testFunc : ()=>{
+    testFunc: () => {
         // this ==> window
         return this.lastName
-    } , 
-    phoneList : ()=>{
+    },
+    phoneList: () => {
         return {
-            homePhone : "01000000",
-            officePhone : "012222222",
-            test : function(){
-                return [10 , 50 ,4]
+            homePhone: "01000000",
+            officePhone: "012222222",
+            test: function () {
+                return [10, 50, 4]
             }
         }
     }
 }
-console.log(user.phoneList())
-console.log(user.phoneList().officePhone)
-console.log(user.phoneList().test())
-console.log(user.phoneList().test()[1])
+// console.log(user.phoneList())
+// console.log(user.phoneList().officePhone)
+// console.log(user.phoneList().test())
+// console.log(user.phoneList().test()[1])
 // console.log(user.age)
 user.homeAddress = "Maadi"
 user['age'] = 25
-console.log(user)
-console.log(user.fullName())
-console.log(user.testFunc()) // mazen
+// console.log(user)
+// console.log(user.fullName())
+// console.log(user.testFunc()) // mazen
 
 
 //   var fullName =  function(){
 //     return "Zein tamer"
 //     }
+
+
+
+var userInfo = {
+    name: "Marwa",
+    age: 40,
+    address: "October"
+}
+
+var userInfo2 = {
+    name: "zein",
+    age: 20,
+    address: "Maadi"
+}
+
+var userInfo3 = {
+    name: "tamer",
+    age: 10,
+    address: "Nasr city"
+}
+
+
+// Array of objects
+var users = [{
+    name: "Marwa",
+    age: 40,
+    address: "October"
+},
+{
+    name: "zein",
+    age: 10,
+    address: "Nasr city"
+},
+{
+    name: "tamer",
+    age: 30,
+    address: "Nasr city"
+}
+]
+
+console.log(users)
+
+users.forEach((item)=>{
+    // console.log(item)
+})
+
+var element = users.find((item)=> item.age == 10)
+var index = users.findIndex((item)=> item.age == 10)
+console.log(element)
+console.log(index)
+
+users[index].address = "new cairo"
+
+console.table(users)
+
+
+
+
