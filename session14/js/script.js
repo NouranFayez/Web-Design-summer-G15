beffc1d26364a14a28146d830fe159740710c278
NouranFayez <nouran_ahmed269@hotmail.com>
2026-09-02 13:48:26 +0300

    session14

--- session14/js/script.js
+++ session14/js/script.js
@@ -46,8 +46,8 @@ function testFunc (){
 
 // localStorage.removeItem("age")
 
-console.log(localStorage.getItem("name"))
-console.log(localStorage.getItem("isGraduated"))
+// console.log(localStorage.getItem("name"))
+// console.log(localStorage.getItem("isGraduated"))
 
 let users = [
     {name : "Ahmed" , email : "Ahmed@email.com"},
@@ -61,12 +61,12 @@ localStorage.setItem('users' , JSON.stringify(users))
 
 
 
-console.log(localStorage.getItem('users'))
+// console.log(localStorage.getItem('users'))
 
 let usersList = JSON.parse(localStorage.getItem('users'))
 usersList.push({name: "Marwa" , email : 'marwa@email.com'})
 
-console.log(usersList)
+// console.log(usersList)
 
 localStorage.setItem('users' , JSON.stringify(usersList))
 
@@ -87,5 +87,52 @@ $(document).ready(function(){
 
 
 // <i class="fa-solid fa-arrow-right"></i>
+let nameValidation = (element)=>{
+    // console.log(element)
+    let inputValue = element.value.trim()
+    console.log(inputValue.length)
 
+    if(inputValue.length < 3)  handleError(element , "enter at least 3 character")
+    else handleError(element)
+}
+
+let handleError= (input , msg = "")=>{
+    input.nextElementSibling.innerText = msg
+}
+
+let emailValiadtion = (element)=>{
+    // https://mailtrap.io/blog/javascript-email-validation/
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    let inputValue = element.value
+    console.log(emailRegex.test(inputValue));
+    (emailRegex.test(inputValue)) ? handleError(element)  
+        : handleError(element , "please enter a valid email");
+
+}
+
+let passwordValidation = (element)=>{
+    // https://regexr.com/3bfsi
+    const passwordReg = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/
+    console.log(passwordReg.test(element.value));
+    (passwordReg.test(element.value)) 
+    ? handleError(element)
+    : handleError(element , "enter at leaset 1 lowercase , 1 uppercase , 1 digit , 1 special character")
+    
+}
+
+
+let myForm = document.getElementById('myForm')
+myForm.addEventListener('input' , (e)=>{
+    // console.log(e)
+    if(e.target.id == "userName") {
+        nameValidation(e.target)
+    }
+    else if(e.target.id == "userEmail") emailValiadtion(e.target)
+    else if(e.target.id == "userPassword") passwordValidation(e.target)
+})
+
+ 
+// name ==> 3 character
+// email ==> username@domain.subdomain
+// password ==> 1 lowercase , 1 uppercase , 1 number , special character (!@#$%^&*)
 
